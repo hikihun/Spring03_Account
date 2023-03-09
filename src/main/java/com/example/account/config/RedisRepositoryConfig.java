717f812acc6b77f250efb16a7d694191213b4684
hikihun <skh140@naver.com>
2023-03-09 20:31:19 +0900

    Redis 실습

---
+++ src/main/java/com/example/account/config/RedisRepositoryConfig.java
@@ -0,0 +1,29 @@
+package com.example.account.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class RedisRepositoryConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
+    @Bean
+    public RedissonClient redssonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort);
+
+        return Redisson.create(config);
+    }
+}
