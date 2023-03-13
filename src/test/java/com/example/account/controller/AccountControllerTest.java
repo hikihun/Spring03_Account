d6ad32098f1f95f7b48de70d44e2e62e5ce10be1
hikihun <skh140@naver.com>
2023-03-13 21:36:35 +0900

    Feat: Add AOP & Test

--- src/test/java/com/example/account/controller/AccountControllerTest.java
+++ src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -7,9 +7,7 @@ import com.example.account.dto.DeleteAccount;
 import com.example.account.exception.AccountException;
 import com.example.account.type.AccountStatus;
 import com.example.account.service.AccountService;
-import com.example.account.service.RedisTestService;
 import com.example.account.type.ErrorCode;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,8 +33,6 @@ class AccountControllerTest {
     @MockBean
     private AccountService accountService;
 
-    @MockBean
-    private RedisTestService redisTestService;
 
     @Autowired
     private MockMvc mockMvc;
